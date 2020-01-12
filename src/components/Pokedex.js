88b26db2268b20b424c73afc41c343eb7a6322b5
Bobby Brice <bobby.brice@gmail.com>
2020-01-11 19:52:45 -0500

    updated css to add transition, transform and hover effects

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -24,7 +24,7 @@ class Pokedex extends Component {
     }
     return (
       <div className="Pokedex">
-        <p>Total Experience: {this.props.exp}</p>
+        <h4>Total Experience: {this.props.exp}</h4>
         {title}
         <div className="Pokedex-cards">
           {this.props.pokemon.map( (p) => (
